fcbc26bc41fd4e3d7e1fe56baf177ac5fa4682f8
Tudor Zugravu <tudorzg@pc-141-18.customer.iris.collegiate.lan>
2017-08-13 21:29:10 +0100

    map tab done

---
+++ Custom-Services/Custom-Services/MapMarkerView.swift
@@ -0,0 +1,27 @@
+//
+//  MapMarkerView.swift
+//  Custom-Services
+//
+//  Created by Tudor Zugravu on 13/08/2017.
+//  Copyright © 2017 Tudor Zugravu. All rights reserved.
+//
+
+import UIKit
+
+class MapMarkerView: UIView {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var timeIntervalLabel: UILabel!
+    @IBOutlet weak var discountLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+ 
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
